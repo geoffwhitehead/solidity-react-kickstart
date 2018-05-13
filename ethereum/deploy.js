8ea12f62b48538ae5348d8337fe3d7e3326b5412
Geoff Whitehead <geoff1012@gmail.com>
2018-05-13 22:13:11 +0100

    adds react and next

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -12,7 +12,6 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log("Attempting to deploy from account", accounts[0]);
-  console.log(accounts);
 
   const result = await new web3.eth.Contract(
     JSON.parse(compiledFactory.interface)
