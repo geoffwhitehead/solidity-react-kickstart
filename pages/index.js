a1166460c2a16f1faeca5376fc607a6746a9ed72
Geoff Whitehead <geoff1012@gmail.com>
2018-05-13 23:52:41 +0100

    creates common layout comp

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory.js";
-import { Card } from "semantic-ui-react";
+import { Card, Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -21,13 +22,17 @@ class CampaignIndex extends Component {
   }
   render() {
     return (
-      <div>
-        <link
-          rel="stylesheet"
-          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
-        />
-        {this.renderCampaigns()}
-      </div>
+      <Layout>
+        <div>
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+          />
+          <h3>Open Campaigns</h3>
+          {this.renderCampaigns()}
+          <Button content="Create Campaign" icon="add circle" primary />
+        </div>
+      </Layout>
     );
   }
 }
