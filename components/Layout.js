80d8067f57a061583ade5132d9f4224a32a4acc6
Geoff Whitehead <geoff1012@gmail.com>
2018-05-14 00:17:14 +0100

    creates new campaign page. adds css to head

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import { Container } from "semantic-ui-react";
+import Head from "next/head";
 
 export default props => {
   return (
     <div>
       <Container>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+          />
+        </Head>
         <Header />
         {props.children}
         <h1>Im a footer</h1>
