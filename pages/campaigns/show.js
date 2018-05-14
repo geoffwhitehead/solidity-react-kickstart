37157e306734b98273463e05242ca1bf3573a63d
Geoff Whitehead <geoff1012@gmail.com>
2018-05-14 20:51:41 +0100

    updates show page by displaying summary details

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
+import { Card } from "semantic-ui-react";
+import web3 from "../../ethereum/web3";
 
 export default class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -11,16 +13,62 @@ export default class CampaignShow extends Component {
     return {
       minimumContribution: summary[0],
       balance: summary[1],
-      requests: summary[2],
+      requestsCount: summary[2],
       approversCount: summary[3],
       manager: summary[4]
     };
+  }
+
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount
+    } = this.props;
+    const items = [
+      {
+        header: manager,
+        meta: "Address of manager",
+        description:
+          "The manager created this campaign and can create requests to withdraw money",
+        style: { overflowWrap: "break-word" }
+      },
+      {
+        header: minimumContribution,
+        meta: "Minimum Contribution (Wei)",
+        description:
+          "You must contribute at least this much Wei to become an approver"
+      },
+      {
+        header: requestsCount,
+        meta: "Number of requests",
+        description:
+          "A requests tries to withdraw money from the contract. Requests must be approved by approvers."
+      },
+      {
+        header: approversCount,
+        meta: "Number of approvers",
+        description:
+          "Number of people who have already donated to this campaign."
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign balance (ether)",
+        description:
+          "The balance is how much money this campaign has left to spend."
+      }
+    ];
+
+    return <Card.Group items={items} />;
   }
 
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
