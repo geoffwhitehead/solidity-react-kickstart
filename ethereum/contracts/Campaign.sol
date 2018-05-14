88bb58bf88879163db609ce0d549b17631159333
Geoff Whitehead <geoff1012@gmail.com>
2018-05-14 20:12:33 +0100

    adds getSummary and getRequestsCount to contract

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -49,11 +49,11 @@ contract Campaign {
     
     function createRequest(string description, uint value, address recipient) public restricted {
         Request memory newRequest = Request({
-           description: description,
-           value: value, 
-           recipient: recipient,
-           complete: false,
-           approvalCount: 0
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
         });
         
         requests.push(newRequest);
@@ -77,5 +77,21 @@ contract Campaign {
         
         request.recipient.transfer(request.value);
         request.complete = true;
+    }
+
+    function getSummary() public view returns (
+        uint, uint, uint, uint, address
+    ) {
+        return (
+            minimumContribution, 
+            this.balance, 
+            requests.length, 
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
     }
 }
