12a05ccbfe478ea59808c10de8fc581d325779f0
Geoff Whitehead <geoff1012@gmail.com>
2018-05-14 20:15:08 +0100

    rebuilt factory

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,9 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x9c145E4EB1D1348ce731104Db3BD2666e64C2643'
+  "0x74Ec4AfdECA730f17618AA6Af34c5C424336F0f0"
 );
 
 export default instance;
