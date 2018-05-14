d4d889d93cc6b03dde49b3de6fb6700920d4a1ea
Geoff Whitehead <geoff1012@gmail.com>
2018-05-14 21:09:33 +0100

    adds functionality to contribute

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
+import Campaign from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
 
 export default class ContributeForm extends Component {
+  state = {
+    value: "",
+    loading: false
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const campaign = Campaign(this.props.address);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, "ether")
+      });
+    } catch (err) {}
+    this.setState({ loading: false });
+  };
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.onSubmit}>
         <Form.Field>
           <label> Amount to contribute</label>
-          <Input label="ether" labelPosition="right" />
+          <Input
+            value={this.state.value}
+            onChange={event => this.setState({ value: event.target.value })}
+            label="ether"
+            labelPosition="right"
+          />
         </Form.Field>
         <Button primary>Contribute</Button>
       </Form>
