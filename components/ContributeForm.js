0672f1de0efe81e5cf20c8ab5a5c42299bae69d5
Geoff Whitehead <geoff1012@gmail.com>
2018-05-14 21:21:13 +0100

    adds loading state and error state to form

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
 import Campaign from "../ethereum/campaign";
 import web3 from "../ethereum/web3";
+import { Router } from "../routes";
 
 export default class ContributeForm extends Component {
   state = {
     value: "",
-    loading: false
+    loading: false,
+    errorMessage: ""
   };
 
   onSubmit = async event => {
     event.preventDefault();
-    this.setState({ loading: true });
     const campaign = Campaign(this.props.address);
+    this.setState({ loading: true, errorMessage: "" });
 
     try {
       const accounts = await web3.eth.getAccounts();
@@ -20,13 +22,16 @@ export default class ContributeForm extends Component {
         from: accounts[0],
         value: web3.utils.toWei(this.state.value, "ether")
       });
-    } catch (err) {}
-    this.setState({ loading: false });
+      Router.replaceRoute(`/campaigns/${this.props.address}`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    this.setState({ loading: false, value: "" });
   };
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label> Amount to contribute</label>
           <Input
@@ -36,7 +41,10 @@ export default class ContributeForm extends Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button primary>Contribute</Button>
+        <Message error header="Oops!" content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
+          Contribute
+        </Button>
       </Form>
     );
   }
